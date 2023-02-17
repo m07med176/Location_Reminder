aabcab56d0a3c31b82fc32826292da0920685f67
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-02-17 03:49:38 +0200

    Test and Android Test

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/PermissionUtils.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/PermissionUtils.kt
@@ -3,30 +3,33 @@ package iti.android.gpslocation
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
-import kotlin.streams.toList
 
 data class Permissions(val permissionName: String, val requestCode:Int)
 class PermissionUtils {
     companion object{
         const val PERMISSION_ID = 520
 
-        fun List<Permissions>.permissionAction(activity: Activity,onPermissionGranted:()->Unit){
+        @RequiresApi(Build.VERSION_CODES.N)
+        fun List<Permissions>.permissionAction(activity: Activity, onPermissionGranted:()->Unit){
             if (checkPermission(activity))
                 onPermissionGranted()
             else
                 requestPermission(activity)
         }
 
+        @RequiresApi(Build.VERSION_CODES.N)
         private fun List<Permissions>.checkPermission(activity: Activity):Boolean
-         =  stream().map { permission->
+        = stream().map { permission->
             ActivityCompat.checkSelfPermission( //ContextCompat
                 activity.applicationContext,
                 permission.permissionName
             ) == PackageManager.PERMISSION_GRANTED
-            }.toList().reduce{a,b -> a && b}
+        }.reduce{a,b -> a && b}.get()
 
-
+        @RequiresApi(Build.VERSION_CODES.N)
         private fun List<Permissions>.requestPermission(activity: Activity) {
             stream().forEach {permission->
                 ActivityCompat.requestPermissions(activity,
